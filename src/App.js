550ab6fe485512a77e18fd6f03a43463944740f5
Chandan Gupta <chandanguptabsb@gmail.com>
2024-04-22 12:38:57 +0000

    Added image to the assets

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./styles.css";
+import ReactBasics from "./components/ReactBasics";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Getting Started with ReactJS</h1>
+      <ReactBasics />
     </div>
   );
 }
---
+++ src/components/ReactBasics.jsx
@@ -0,0 +1,11 @@
+import projectIntro from "../assets/react-project-intro.png";
+
+export default function ReactBasics() {
+  return (
+    <img
+      src={projectIntro}
+      alt="React project introduction"
+      style={{ width: "500px" }}
+    />
+  );
+}
